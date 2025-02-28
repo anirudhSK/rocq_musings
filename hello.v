6fb19b676ccb1c458b805de5ad826dd63481d3f6
anirudhsk <sk.anirudh@gmail.com>
2025-02-28 17:47:12 -0500

    Add variables to expressions.

--- hello.v
+++ hello.v
@@ -1,34 +1,46 @@
 Require Import Arith.
+From Coq Require Import Strings.String.
 
 (* Simple data type of expressions *)
 Inductive expr : Type :=
   | Constant (n1 : nat)
   | Plus (e1 e2 : expr)
   | Minus (e1 e2 : expr)
-  | Mul (e1 e2 : expr).
+  | Mul (e1 e2 : expr)
+  | Var (name : string).
+
+(* State of the machine with current values of variables *)
+Definition state := string -> nat.
+
+(* Empty state with no current values *)
+Definition empty_state (s : string) :=
+  match s with
+    | _ =>0
+  end.
 
 (* Function to evaluate expressions *)
-Fixpoint eval_expr(e: expr) :=
+Fixpoint eval_expr (e: expr) (s : state) :=
   match e with
     | Constant n => n
-    | Plus e1 e2 => (eval_expr e1) + (eval_expr e2)
-    | Minus e1 e2 => (eval_expr e1) - (eval_expr e2)
-    | Mul e1 e2 => (eval_expr e1) * (eval_expr e2)
-  end.  
+    | Plus e1 e2 => (eval_expr e1 s) + (eval_expr e2 s)
+    | Minus e1 e2 => (eval_expr e1 s) - (eval_expr e2 s)
+    | Mul e1 e2 => (eval_expr e1 s) * (eval_expr e2 s)
+    | Var name => s name
+  end.
 
 (* Evaluate expressions*)
-Compute (eval_expr (Plus (Constant 5) (Constant 6))).
-Compute (eval_expr (Minus (Constant 6) (Constant 10))).
-Compute (eval_expr (Minus (Constant 6) (Constant 2))).
-Compute (eval_expr (Mul (Constant 6) (Constant 2))).
+Compute (eval_expr (Plus (Constant 5) (Constant 6)) empty_state).
+Compute (eval_expr (Minus (Constant 6) (Constant 10)) empty_state).
+Compute (eval_expr (Minus (Constant 6) (Constant 2)) empty_state).
+Compute (eval_expr (Mul (Constant 6) (Constant 2)) empty_state).
 
 (* A*(B+C) = AB + AC *)
 Example distribute:
-forall a b c: expr, 
-  eval_expr (Mul a (Plus b c)) =
-  eval_expr (Mul a b) + eval_expr (Mul a c).
+forall (a b c : expr) (s : state), 
+  eval_expr (Mul a (Plus b c)) s =
+  (eval_expr (Mul a b) s) + (eval_expr (Mul a c) s).
 Proof.
- intros a b c.
+ intros a b c s.
  destruct a, b, c; simpl; ring.
  Qed.
 
@@ -42,8 +54,8 @@ Definition constant_fold(e : expr):=
 end.
 
 (* Prove correctness of constant_fold *)
-Theorem constant_fold_thm : forall e,
-   eval_expr (constant_fold e) =  eval_expr e.
+Theorem constant_fold_thm : forall (e : expr) (s : state),
+   eval_expr (constant_fold e) s =  eval_expr e s.
 Proof.
   destruct e; try reflexivity || destruct e1, e2; reflexivity.
   (* TODO: induction e also seems to work here, ask JoeT *)
