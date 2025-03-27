6d4cce9f9c03cd5f4ed35a6f4534623cb077bd1f
anirudhsk <sk.anirudh@gmail.com>
2025-03-27 16:43:21 -0400

    Some more structure around the Smt equivalence checker.

--- SmtEquivalenceChecker.v
+++ SmtEquivalenceChecker.v
@@ -10,19 +10,18 @@ Definition symbolize_expr (e : expr) : smt_expr := Empty.
 
 Definition sym_plus (e1 e2 : smt_expr) : smt_expr := Empty.
 
-Definition check_sym_equality (e1 e2 : smt_expr) : bool := false.
+(* Function signature for Z3 equivalence checker *)
+Parameter sym_checker :  smt_expr -> smt_expr -> bool.
 
-Definition fn (e1 e2 : smt_expr) : bool.
-Admitted. (* Is this reasonable programming in Coq? *)
-
-Check fn.
+(* Soundness of Z3: if the symbolic versions of 2 exprs are shown equivalent by z3, then the 2 exprs are equivalent *)
+Axiom sound_sym_checker : forall e1 e2 : expr, sym_checker (symbolize_expr e1) (symbolize_expr e2) = true -> forall s, aequiv e1 e2 s.
 
 (* And prove it equivalent in the same way ... *)
 Definition smt_equivalence_checker (e1 e2 : expr) (s : state) : bool := 
   match e1, e2 with
     | Constant n1, Constant n2 => Nat.eqb n1 n2
     | Var name1, Var name2 => String.eqb name1 name2
-    | Plus e11 e12, Plus e21 e22 => check_sym_equality (sym_plus (symbolize_expr e11) (symbolize_expr e12))
-                                                       (sym_plus (symbolize_expr e21) (symbolize_expr e22))
+    | Plus e11 e12, Plus e21 e22 => sym_checker (sym_plus (symbolize_expr e11) (symbolize_expr e12))
+                                                (sym_plus (symbolize_expr e21) (symbolize_expr e22))
     | _, _ => false
   end.
