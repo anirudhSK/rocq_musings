e18e73675b28f9f7972aa53e3c0084bc4eca9674
anirudhsk <sk.anirudh@gmail.com>
2025-03-27 12:08:06 -0400

    Some thoughts on SmtEquivalenceChecker.

--- SmtEquivalenceChecker.v
+++ SmtEquivalenceChecker.v
@@ -1,2 +1,21 @@
-(* TODO: Write an SMT-based equivalence checker here. *)
+From MyProject Require Export Expr.
+
+Inductive smt_expr : Type :=
+  | SymConstant (n1 : nat)
+  | Empty.
+
+Definition symbolize_expr (e : expr) : smt_expr := Empty.
+
+Definition sym_plus (e1 e2 : smt_expr) : smt_expr := Empty.
+
+Definition check_sym_equality (e1 e2 : smt_expr) : bool := false.
+
 (* And prove it equivalent in the same way ... *)
+Definition smt_equivalence_checker (e1 e2 : expr) (s : state) : bool := 
+  match e1, e2 with
+    | Constant n1, Constant n2 => Nat.eqb n1 n2
+    | Var name1, Var name2 => String.eqb name1 name2
+    | Plus e11 e12, Plus e21 e22 => check_sym_equality (sym_plus (symbolize_expr e11) (symbolize_expr e12))
+                                                       (sym_plus (symbolize_expr e21) (symbolize_expr e22))
+    | _, _ => false
+  end.
