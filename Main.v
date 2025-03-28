8350f39c28e8da2a2980a5a25448f2e1f2553a2f
anirudhsk <sk.anirudh@gmail.com>
2025-03-28 06:42:37 -0400

    Streamline syntactic equivalence checker proofs.

--- Main.v
+++ Main.v
@@ -2,11 +2,11 @@ Require Import Arith.
 Require Import Bool.
 From Coq Require Import Strings.String.
 Open Scope string_scope.
-From MyProject Require Export EquivalenceChecker.
-From MyProject Require Export SmtEquivalenceChecker.
+From MyProject Require Export SynEquivChecker.
+From MyProject Require Export SmtEquivChecker.
 
-Lemma one_more_lemma (e1 e2 : expr)  (s : state) :
-  equivalence_checker e1 e2 s = true -> e1 = e2.
+Lemma sound_syn_checker (e1 e2 : expr)  (s : state) :
+  syn_equiv_checker e1 e2 s = true -> e1 = e2.
   Proof.
     intros H.
     revert H.
@@ -38,77 +38,15 @@ Lemma one_more_lemma (e1 e2 : expr)  (s : state) :
       reflexivity.
   Qed.
 
-Lemma plus_lemma: (forall (e1_1 e1_2 e2_1 e2_2 : expr) (s : state), 
-  equivalence_checker e1_1 e2_1 s &&
-  equivalence_checker e1_2 e2_2 s = true -> Plus e1_1 e1_2 = Plus e2_1 e2_2).
-  Proof.
-    intros.
-    apply Bool.andb_true_iff in H.
-    destruct H.
-    apply one_more_lemma in H.
-    apply one_more_lemma in H0.
-    rewrite H.
-    rewrite H0.
-    reflexivity.
-Qed.
-
-Lemma minus_lemma: (forall (e1_1 e1_2 e2_1 e2_2 : expr) (s : state), 
-  equivalence_checker e1_1 e2_1 s &&
-  equivalence_checker e1_2 e2_2 s = true -> Minus e1_1 e1_2 = Minus e2_1 e2_2).
-  Proof.
-    intros.
-    apply Bool.andb_true_iff in H.
-    destruct H.
-    apply one_more_lemma in H.
-    apply one_more_lemma in H0.
-    rewrite H.
-    rewrite H0.
-    reflexivity.
-Qed.
-
-
-Lemma mul_lemma: (forall (e1_1 e1_2 e2_1 e2_2 : expr) (s : state), 
-  equivalence_checker e1_1 e2_1 s &&
-  equivalence_checker e1_2 e2_2 s = true -> Mul e1_1 e1_2 = Mul e2_1 e2_2).
-  Proof.
-    intros.
-    apply Bool.andb_true_iff in H.
-    destruct H.
-    apply one_more_lemma in H.
-    apply one_more_lemma in H0.
-    rewrite H.
-    rewrite H0.
-    reflexivity.
-Qed.
-
-Lemma lemma1 :
-  forall (e1 e2 : expr) (s : state), equivalence_checker e1 e2 s = true -> e1 = e2.
-  Proof.
-    intros e1 e2 s.
-    unfold equivalence_checker.
-    destruct e1, e2; try discriminate.
-    - intros H.
-      apply Nat.eqb_eq in H.
-      rewrite H.
-      reflexivity.
-    - apply plus_lemma.
-    - apply minus_lemma.
-    - apply mul_lemma.
-    - intros H.
-      apply String.eqb_eq in H.
-      rewrite H.
-      reflexivity.
-Qed.
-
 (* Prove that the equivalence checker is correct *)
-Theorem equivalence_checker_correct:
-  forall (e1 e2 : expr) (s : state), equivalence_checker e1 e2 s = true -> aequiv e1 e2 s.
+Theorem syn_equiv_checker_correct:
+  forall (e1 e2 : expr) (s : state), syn_equiv_checker e1 e2 s = true -> aequiv e1 e2 s.
 Proof.
 intros e1 e2.
 unfold aequiv.
 intros s.
 intros H.
-apply lemma1 in H.
+apply sound_syn_checker in H.
 rewrite H.
 reflexivity.
 Qed.
@@ -130,28 +68,28 @@ Lemma smt_plus_lemma:
  Qed.
 
 (* Prove that the SMT equivalence checker is correct *)
-Theorem smt_equivalence_checker_correct:
-  forall (e1 e2 : expr) (s : state), smt_equivalence_checker e1 e2 s = true -> aequiv e1 e2 s.
+Theorem smt_equiv_checker_correct:
+  forall (e1 e2 : expr) (s : state), smt_equiv_checker e1 e2 s = true -> aequiv e1 e2 s.
 Proof.
   intros e1 e2 s.
   unfold aequiv.
   intros H.
   destruct e1, e2; try discriminate.
-  - unfold smt_equivalence_checker in H.
+  - unfold smt_equiv_checker in H.
     apply Nat.eqb_eq in H.
     rewrite H.
     reflexivity.
-  - unfold smt_equivalence_checker in H.
-    apply sound_sym_checker.
+  - unfold smt_equiv_checker in H.
+    apply sound_smt_checker.
     rewrite smt_plus_lemma.
     rewrite smt_plus_lemma.
     apply H.
-  - unfold smt_equivalence_checker in H.
-    apply sound_sym_checker.
+  - unfold smt_equiv_checker in H.
+    apply sound_smt_checker.
     rewrite smt_minus_lemma.
     rewrite smt_minus_lemma.
     apply H.
-  - unfold smt_equivalence_checker in H.
+  - unfold smt_equiv_checker in H.
     apply String.eqb_eq in H.
     rewrite H.
     reflexivity.
--- SmtEquivChecker.v
+++ SmtEquivChecker.v
@@ -21,10 +21,10 @@ Fixpoint symbolize_expr (e : expr) : smt_expr :=
 Parameter sym_checker :  smt_expr -> smt_expr -> bool.
 
 (* Soundness of Z3: if the symbolic versions of 2 exprs are shown equivalent by z3, then the 2 exprs are equivalent *)
-Axiom sound_sym_checker : forall e1 e2 : expr, sym_checker (symbolize_expr e1) (symbolize_expr e2) = true -> forall s, aequiv e1 e2 s.
+Axiom sound_smt_checker : forall e1 e2 : expr, sym_checker (symbolize_expr e1) (symbolize_expr e2) = true -> forall s, aequiv e1 e2 s.
 
 (* And prove it equivalent in the same way ... *)
-Definition smt_equivalence_checker (e1 e2 : expr) (s : state) : bool := 
+Definition smt_equiv_checker (e1 e2 : expr) (s : state) : bool := 
   match e1, e2 with
     | Constant n1, Constant n2 => Nat.eqb n1 n2
     | Var name1, Var name2 => String.eqb name1 name2
