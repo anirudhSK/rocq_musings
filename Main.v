d2725ae8e2cfd3feb32d1e8c8aa964f5545ead7f
anirudhsk <sk.anirudh@gmail.com>
2025-04-05 06:46:11 -0700

    Remove old syntactic checker and lemmas that are no longer useful.

--- Main.v
+++ Main.v
@@ -2,80 +2,9 @@ Require Import Arith.
 Require Import Bool.
 From Coq Require Import Strings.String.
 Open Scope string_scope.
-From MyProject Require Export SynEquivChecker.
 From MyProject Require Export SmtEquivChecker.
 
-Lemma sound_syn_checker (e1 e2 : expr)  (s : state) :
-  syn_equiv_checker e1 e2 s = true -> e1 = e2.
-  Proof.
-    intros H.
-    revert H.
-    revert e2.
-    induction e1; intros e2 H; destruct e2; try discriminate.
-    - apply Nat.eqb_eq in H.
-      rewrite H.
-      reflexivity.
-    - simpl in H.
-      apply Bool.andb_true_iff in H.
-      destruct H.
-      rewrite (IHe1_1 _ H).
-      rewrite (IHe1_2 e2_2 H0).
-      reflexivity.
-    - simpl in H.
-      apply Bool.andb_true_iff in H.
-      destruct H.
-      rewrite (IHe1_1 _ H).
-      rewrite (IHe1_2 e2_2 H0).
-      reflexivity.
-    - simpl in H.
-      apply Bool.andb_true_iff in H.
-      destruct H.
-      rewrite (IHe1_1 _ H).
-      rewrite (IHe1_2 e2_2 H0).
-      reflexivity.
-    - apply String.eqb_eq in H.
-      rewrite H.
-      reflexivity.
-  Qed.
-
-(* Prove that the equivalence checker is correct *)
-Theorem syn_equiv_checker_correct:
-  forall (e1 e2 : expr) (s : state), syn_equiv_checker e1 e2 s = true -> aequiv e1 e2 s.
-Proof.
-intros e1 e2.
-unfold aequiv.
-intros s.
-intros H.
-apply sound_syn_checker in H.
-rewrite H.
-reflexivity.
-Qed.
-
-Lemma smt_plus_lemma:
- forall (e1 e2 : expr),
- symbolize_expr (Plus e1 e2) = SymPlus (symbolize_expr e1) (symbolize_expr e2).
- Proof.
-  intros.
-  destruct e1, e2; try unfold symbolize_expr; reflexivity.
- Qed.
-
- Lemma smt_minus_lemma:
- forall (e1 e2 : expr),
- symbolize_expr (Minus e1 e2) = SymMinus (symbolize_expr e1) (symbolize_expr e2).
- Proof.
-  intros.
-  destruct e1, e2; try unfold symbolize_expr; reflexivity.
- Qed.
-
-(* Prove that the SMT equivalence checker is correct *)
-Theorem smt_equiv_checker_correct:
-  forall (e1 e2 : expr) (s : state), smt_equiv_checker e1 e2 s = true -> aequiv e1 e2 s.
-Proof.
-  apply sound_smt_checker.
-Qed.
-
-(* TODO: Write a constant fold optimizer before passing stuff into the z3 solver *)
-(* Kind of like the optimizations in the K2 project that speeded up equivalence checking *)
+(* Kind of like the constant folding optimizations in the K2 project that speeded up equivalence checking *)
 Definition constant_fold(e : expr):=
   match e with
   | Plus (Constant n1) (Constant n2) => Constant (n1 + n2)
