584a335633838a748e51ddfb5e9c13bbdffd91ad
anirudhsk <sk.anirudh@gmail.com>
2025-05-26 17:05:27 -0400

    Clean up code: makes it more similar to other inductive definitions.

--- CrParser.v
+++ CrParser.v
@@ -9,18 +9,18 @@ Definition Parser : Type := list ParserState.
 
 (* An ExtractOp pulls out a Header starting at index Start and ending at index End *)
 Inductive ExtractOp : Type := 
-  | ExtractOpConstructor : Header -> nat -> nat -> ExtractOp.
+  | ExtractOpConstructor (h : Header) (start_index : nat) (end_index : nat).
 
 (* A Transition is either:
    (1) an unconditional jump to another parser state
    (2) a conditional jump to another parser state,
        conditional on the bits between index Start and index End of a particular Header matching a bit-pattern Pat *)
 Inductive Transition : Type :=
-  | Unconditional : ParserState -> Transition
-  | Conditional : Header -> nat -> nat -> list bool -> ParserState -> Transition.
+  | Unconditional (target : ParserState)
+  | Conditional (h : Header) (start_index : nat) (end_index : nat) (pattern : list bool) (target : ParserState).
 
 (* A ParserStateDef is a pair consisting of 
  (1) an ExtractOp in that state, AND
  (2) a Transition out of that state *)
 Inductive ParserStateDef : Type := 
-  | ParserStateDefConstructor : ParserState -> ExtractOp -> Transition -> ParserStateDef.
+  | ParserStateDefConstructor (s : ParserState) (e : ExtractOp) (t: Transition).
