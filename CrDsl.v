280740532995f174b24001ebd00469975c977ad1
anirudhsk <sk.anirudh@gmail.com>
2025-06-05 22:19:46 -0400

    Remove parser and scheduler sub DSLs.
    Will put these back in once we finish the transformer DSL.

--- CrDsl.v
+++ CrDsl.v
@@ -6,15 +6,11 @@ Require Import List.
 Import ListNotations.
 Require Import Strings.String.
 From MyProject Require Export CrIdentifiers.
-From MyProject Require Export CrParser.
 From MyProject Require Export CrTransformer.
-From MyProject Require Export CrScheduler.
 
 (* A Module has a module name and either a parser or transformer definition *)
 Inductive Module : Type := 
-  | ParserModule (m : ModuleName) (p : Parser)
-  | TransformerModule (m : ModuleName) (t : Transformer)
-  | SchedulerModule (m : ModuleName) (s : Scheduler).
+  | TransformerModule (m : ModuleName) (t : Transformer).
 
 (* A Connection is a pair of module names *)
 (* and a connection name *)
