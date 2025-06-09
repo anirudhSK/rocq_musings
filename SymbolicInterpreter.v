cd492eb92de3dbc11c86e6e363a44ea3f4a0adda
anirudhsk <sk.anirudh@gmail.com>
2025-06-09 11:36:35 -0400

    Check in some more code for the symbolic interpreter.

--- SymbolicInterpreter.v
+++ SymbolicInterpreter.v
@@ -1,22 +1,34 @@
 From MyProject Require Import CrTransformer.
+From MyProject Require Import CrIdentifiers.
 From MyProject Require Import SmtExpr.
 Require Import ZArith.
+Require Import Coq.Strings.String.
+
+(* Some preprocessing operations before we begin the transformation to SMT.
+   For now, leave these as function signatures. 
+   The idea is that if these return true, then we worry about correctness of the transformation to SMT,
+   else, we just error out right here. *)
+Parameter check_uninitilized : Transformer -> bool.
+Parameter check_unique : Transformer -> bool.
+
+(* Convert FunctionArgument to SmtExpr *)
+Definition fn_arg_to_smt_expr (arg : FunctionArgument) : SmtExpr :=
+    match arg with
+    | HeaderArg (HeaderCtr h) => SmtVar ("hdr_" ++ h)
+    | CtrlPlaneArg (CtrlPlaneConfigNameCtr c) => SmtVar ("ctrl_" ++ c)
+    | ConstantArg n => SmtConst n
+    | StatefulArg (StateVarCtr s) => SmtVar ("state_" ++ s)
+    end.
 
 (* Define the symbolic interpreter for header operations *)
 Definition symbolic_interpreter (h : HdrOp) : SmtExpr :=
     match h with
     | StatefulOp f arg1 arg2 target =>
        match f with 
-         | AddOp => SmtConst zero (*TODO: placeholder *)
+         | AddOp => SmtBitAdd (fn_arg_to_smt_expr arg1) (fn_arg_to_smt_expr arg2)
        end
     | StatelessOp f arg1 arg2 target =>
        match f with
-         | AddOp => SmtConst zero (*TODO: placeholder *)
+         | AddOp => SmtBitAdd (fn_arg_to_smt_expr arg1) (fn_arg_to_smt_expr arg2)
        end
     end.
-
-(* TODO: Need to handle arg1 and arg2 being various symbolic types: HeaderArg, CtrlPlaneArg,
-   and we need to introduce unique string names for each of these things so that the smt formula does the right thing *)
-
-(* TODO: Need to compile down the AddOp and MulOp down into
-   the basic ops (and, or, not) we support in SmtExpr *)
