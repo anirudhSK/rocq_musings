6b4504a6e91080ae4cbf52a7475bdab9d0e9d2b0
Anirudh Sivaraman <sk.anirudh@gmail.com>
2025-06-18 17:45:17 -0400

    Use positive maps which actually have a non-mathematical-function-style map implementation.

--- test_maps.v
+++ test_maps.v
@@ -1,5 +1,6 @@
 From MyProject Require Import Maps.
 From Coq.Strings Require Import String.
+Require Import Coq.PArith.PArith.
 
 Check string_dec : forall (x y : string), {x = y} + {x <> y}.
 
@@ -9,3 +10,13 @@ Module StringEq.
 End StringEq.
 
 Module StringMap := EMap(StringEq).
+
+Definition test_map : StringMap.t nat.
+
+Example test_pmap  := PMap.init nat.
+
+Eval compute in PMap.init 0.
+
+Eval compute in (PMap.set (Pos.of_nat 1) 5 (PMap.init 0)).
+
+Eval compute in (PMap.get (Pos.of_nat 1) (PMap.set (Pos.of_nat 1) 5 (PMap.init 0))).
