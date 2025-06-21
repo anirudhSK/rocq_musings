40efad56f8dfffd2443a1477736f769fc5067884
anirudhsk <sk.anirudh@gmail.com>
2025-06-21 09:21:05 -0400

    Add eval function for bools.
    Remove some operations that we don't need for now.

--- SmtExpr.v
+++ SmtExpr.v
@@ -5,6 +5,8 @@ From Coq.Strings Require Import String.
 
 (* Note that these strings may or may not have a one-to-one correspondence with
   identifiers in the CrDsl program. *)
+(* Currently only need valuations from strings to uint8 because there are no bools in IR. *)
+(* TODO: Does that sound reasonable overall? *)
 Definition SmtValuation := string -> uint8.
 
 (* Basic starting point of Smt expressions, just have 8 bit ands, ors, and nots.
@@ -19,7 +21,6 @@ Inductive SmtArithExpr :=
     | SmtBitAnd (e1 e2 : SmtArithExpr)
     | SmtBitOr (e1 e2 : SmtArithExpr)
     | SmtBitXor (e1 e2 : SmtArithExpr)
-    | SmtBitEq (e1 e2 : SmtArithExpr)
     | SmtBitNot (e : SmtArithExpr)
     | SmtBitMul (e1 e2 : SmtArithExpr)
     | SmtBitDiv (e1 e2 : SmtArithExpr)
@@ -28,13 +29,10 @@ Inductive SmtArithExpr :=
 Inductive SmtBoolExpr :=
     | SmtTrue
     | SmtFalse
-    | SmtBoolVar (name : string)
     | SmtBoolNot (e : SmtBoolExpr)
     | SmtBoolAnd (e1 e2 : SmtBoolExpr)
     | SmtBoolOr (e1 e2 : SmtBoolExpr)
-    | SmtBoolEq  (e1 e2 : SmtArithExpr)
-    | SmtBoolLeq (e1 e2 : SmtArithExpr)
-    | SmtBoolGeq (e1 e2 : SmtArithExpr).   
+    | SmtBoolEq  (e1 e2 : SmtArithExpr).   
 
 (* Provide semantics for the SmtArithExpr above using the uint8 functions from Integer.v *)
 Fixpoint eval_smt_arith (e : SmtArithExpr) (v : SmtValuation) : uint8 :=
@@ -49,9 +47,18 @@ Fixpoint eval_smt_arith (e : SmtArithExpr) (v : SmtValuation) : uint8 :=
     | SmtBitAnd e1 e2 => and (eval_smt_arith e1 v) (eval_smt_arith e2 v)
     | SmtBitOr e1 e2 => or (eval_smt_arith e1 v) (eval_smt_arith e2 v)
     | SmtBitXor e1 e2 => xor  (eval_smt_arith e1 v) (eval_smt_arith e2 v)
-    | SmtBitEq e1 e2 => if (eq  (eval_smt_arith e1 v) (eval_smt_arith e2 v)) then one else zero (* Is this one = 1 or 255? Does it matter? *)
     | SmtBitNot e => not (eval_smt_arith e v)
     | SmtBitMul e1 e2 => mul (eval_smt_arith e1 v) (eval_smt_arith e2 v)
     | SmtBitDiv e1 e2 => divu  (eval_smt_arith e1 v) (eval_smt_arith e2 v)
     | SmtBitMod e1 e2 => modu  (eval_smt_arith e1 v) (eval_smt_arith e2 v)
+    end.
+
+Fixpoint eval_smt_bool (e : SmtBoolExpr) (v : SmtValuation) : bool :=
+    match e with
+    | SmtTrue => true
+    | SmtFalse => false
+    | SmtBoolNot e' => negb (eval_smt_bool e' v)
+    | SmtBoolAnd e1 e2 => andb (eval_smt_bool e1 v) (eval_smt_bool e2 v)
+    | SmtBoolOr e1 e2 => orb (eval_smt_bool e1 v) (eval_smt_bool e2 v)
+    | SmtBoolEq e1 e2 => if (eq (eval_smt_arith e1 v) (eval_smt_arith e2 v)) then true else false
     end.
