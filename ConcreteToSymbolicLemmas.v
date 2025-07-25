e179388f1909ed1581e54be4d119b9bab205377f
anirudhsk <sk.anirudh@gmail.com>
2025-07-24 21:38:46 -0400

    Revert "Admit these lemmas, turns out only these are left."
    This reverts commit 3f303f01754459dd23e08124290cae978789282b.

--- ConcreteToSymbolicLemmas.v
+++ ConcreteToSymbolicLemmas.v
@@ -630,8 +630,10 @@ Proof.
   intro h.
   unfold eval_transformer_uint8.
   remember (find_first_match (combine (get_match_results t (eval_sym_state s1 f)) t)) as concrete_match.
-  destruct concrete_match eqn:des; admit.
-Admitted.
+  destruct concrete_match eqn:des.
+  - simpl. apply switch_case_expr_some_match_lemma. assumption.
+  - simpl. apply switch_case_expr_no_match_lemma. assumption.
+Qed.
 
 Lemma commute_sym_vs_conc_transformer_state_var_map:
   forall t f s1,
@@ -644,7 +646,9 @@ Proof.
   unfold eval_transformer_uint8.
   remember (find_first_match (combine (get_match_results t (eval_sym_state s1 f)) t)) as concrete_match.
   destruct concrete_match eqn:des.
-Admitted.
+  - simpl. apply switch_case_expr_some_match_state_var_lemma. assumption.
+  - simpl. apply switch_case_expr_no_match_state_var_lemma. assumption.
+Qed.
 
 Lemma commute_sym_vs_conc_transformer_ctrl_plane_map:
   forall t f s1,
