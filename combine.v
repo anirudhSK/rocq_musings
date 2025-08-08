3e160ca6aa5b83c9d29c27655190206a3237fc62
LanderDuncan <134824437+LanderDuncan@users.noreply.github.com>
2025-08-08 11:08:45 -0400

    first generated

---
+++ combine.v
@@ -0,0 +1,36 @@
+From MyProject Require Import first_generated.
+From MyProject Require Import second_generated.
+From MyProject Require Import SmtExpr.
+From MyProject Require Import CrProgramState.
+From MyProject Require Import CrTransformer.
+Require Import ZArith.
+Require Import List.
+Import ListNotations.
+From MyProject Require Import SmtQuery.
+
+
+(* THE SIMPLEST POSSIBLE ProgramState SmtArithExpr *)
+(* We have been operating on stateless only so far so this is a dummy instance to satisfy the compiler.*)
+Definition simplest_state : ProgramState SmtArithExpr := {|
+  ctrl_map := PMap.init (SmtConst (repr 0%Z));
+  header_map := PMap.init (SmtConst (repr 0%Z));
+  state_map := PMap.init (SmtConst (repr 0%Z))
+|}.
+
+
+Definition headers_to_check : list Header := [first_generated.h_a; second_generated.h_a; first_generated.h_b; second_generated.h_b].
+Definition state_vars_to_check : list State := [].
+
+Definition transformer_first: Transformer := [first_generated.the_table_0_rule].
+Definition transformer_second: Transformer := [second_generated.the_table_0_rule].
+
+(* Construct the equivalence checker instance *)
+Definition my_equivalence_check : SmtResult :=
+  equivalence_checker
+      simplest_state       (* Starting symbolic state *)
+      transformer_first         (* First rule *)
+      transformer_second          (* Second rule (same) *)
+      headers_to_check             
+      state_vars_to_check.   
+
+Eval compute in my_equivalence_check.
