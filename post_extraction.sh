bd7eebb6b340cc75b9770ffbd1a1da131a20b516
Anirudh Sivaraman <sk.anirudh@gmail.com>
2025-08-12 15:49:18 -0400

    Steps to produce an actual executable.

--- post_extraction.sh
+++ post_extraction.sh
@@ -1,3 +1,4 @@
 ocamlfind ocamlc -thread -package z3 -linkpkg -c Z3Example.ml
 ocamlc -c EquivalenceChecker.mli
 ocamlc -c EquivalenceChecker.ml
+ocamlfind ocamlc -thread -package z3 -linkpkg Z3Example.cmo EquivalenceChecker.cmo -o eqchecker
