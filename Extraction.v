dbc625538b3db2059c5db1648d27787a5b1da0e0
Anirudh Sivaraman <sk.anirudh@gmail.com>
2025-08-12 12:27:12 -0400

    Some more improvements to extraction.

--- Extraction.v
+++ Extraction.v
@@ -5,7 +5,8 @@ From MyProject Require Import SmtQuery.
 
 (* Tell extraction to use your external OCaml implementation *)
 Extract Constant smt_query => "fun expr -> match expr with | _ ->
-                               SmtUnknown".
+                               SmtUnknown
+                               let y = let open Z3Example in Z3Example.example_1".
 
 (* Extract everything else normally *)
 Extraction "EquivalenceChecker.ml" SmtQuery.equivalence_checker_cr_dsl.
--- post_extraction.sh
+++ post_extraction.sh
@@ -1,1 +1,3 @@
-ocamlc -o eq_checker EquivalenceChecker.ml
+ocamlfind ocamlc -thread -package z3 -linkpkg -c Z3Example.ml
+ocamlc -c EquivalenceChecker.mli
+ocamlc -c EquivalenceChecker.ml
