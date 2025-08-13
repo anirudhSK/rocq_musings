db28184b498bef27a0c4dab20851f41f73a6ba11
Anirudh Sivaraman <sk.anirudh@gmail.com>
2025-08-13 13:41:16 -0400

    Some improvements to extraction after discussing with Joe.

--- Extraction.v
+++ Extraction.v
@@ -4,11 +4,9 @@ Extraction Language OCaml.
 From MyProject Require Import SmtQuery.
 
 (* Tell extraction to use your external OCaml implementation *)
-Extract Constant smt_query => "fun expr -> match expr with | _ ->
-                               SmtUnknown
-                               let y = let open Z3Example in Z3Example.example_1".
+Extract Constant smt_query => "MyZ3.solve". (* TODO: Implement MyZ3.solve *)
 
-Extraction "SmtExpr.ml" SmtExpr.SmtBoolExpr SmtQuery.SmtResult.
+Set Extraction Output Directory "extracted_code".
 
 (* Extract everything else normally *)
-Extraction "EquivalenceChecker.ml" SmtQuery.equivalence_checker_cr_dsl.
+Separate Extraction SmtQuery.equivalence_checker_cr_dsl.
