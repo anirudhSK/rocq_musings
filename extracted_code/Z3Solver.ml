e3ce7b814238f729eb4d587bebaee80070405f1b
Sean Rhee <scr8455@mac.mynetworksettings.com>
2025-09-22 16:15:22 -0400

    working s-expression type interface

--- extracted_code/Z3Solver.ml
+++ extracted_code/Z3Solver.ml
@@ -1,26 +1,47 @@
 open Z3
 
 module Datatypes = struct
-
-(* If you omit this next line, then we would lose all the other
-   operations in the new re-defined Datatypes, so first we include
-   it. *)
-include Datatypes
-
-type bool = [%import: Datatypes.bool]
-[@@deriving sexp]
-type 'a list = [%import: 'a Datatypes.list]
-[@@deriving sexp]
+  (* If you omit this next line, then we would lose all the other
+    operations in the new re-defined Datatypes, so first we include
+    it. *)
+  include Datatypes
+  type bool = [%import: Datatypes.bool]
+  [@@deriving sexp]
 end
 module Ascii = struct
-include Ascii
-type ascii = [%import: Ascii.ascii]
-[@@deriving sexp]
+  include Ascii
+  type ascii = [%import: Ascii.ascii]
+  [@@deriving sexp]
 end
-
-(* Check that we can still use length *)
-
-let _ = Datatypes.length (Datatypes.Coq_nil)
+module String = struct
+  include String
+  type string = [%import: String.string]
+  [@@deriving sexp]
+end
+module BinNums = struct
+  include BinNums
+  type positive = [%import: BinNums.positive]
+  [@@deriving sexp]
+  type coq_Z = [%import: BinNums.coq_Z]
+  [@@deriving sexp]
+end
+module Integers = struct
+  include Integers
+  type bit_int = [%import: Integers.bit_int]
+  [@@deriving sexp]
+end
+module MyInts = struct
+  include MyInts
+  type uint8 = [%import: MyInts.uint8]
+  [@@deriving sexp]
+end
+module SmtExpr = struct
+  include SmtExpr
+  type coq_SmtBoolExpr = [%import: SmtExpr.coq_SmtBoolExpr]
+  [@@deriving sexp]
+  and coq_SmtArithExpr = [%import: SmtExpr.coq_SmtArithExpr]
+  [@@deriving sexp]
+end
 
 (* Recursively convert a coq_SmtBoolExpr to a Z3 expression *)
 (* TODO: This function is trusted, so needs to be checked via other means like fuzzing *)
