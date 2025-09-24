2cb46dd5e7f789e130e0d9b7befb7ac772d28da2
Sean Rhee <scr8455@mac.mynetworksettings.com>
2025-09-24 12:43:16 -0400

    CaracaraProgram S-Expression bindings

--- extracted_code/Interface.ml
+++ extracted_code/Interface.ml
@@ -1,26 +1,15 @@
-module Datatypes = struct
-  (* If you omit this next line, then we would lose all the other
-    operations in the new re-defined Datatypes, so first we include
-    it. *)
-  include Datatypes
-  type bool = [%import: Datatypes.bool]
-  [@@deriving sexp]
-end
-module Ascii = struct
-  include Ascii
-  type ascii = [%import: Ascii.ascii]
-  [@@deriving sexp]
-end
-module String = struct
-  include String
-  type string = [%import: String.string]
-  [@@deriving sexp]
-end
 module BinNums = struct
   include BinNums
   type positive = [%import: BinNums.positive]
   [@@deriving sexp]
   type coq_Z = [%import: BinNums.coq_Z]
+  [@@deriving sexp]
+end
+module Datatypes = struct
+  include Datatypes
+  type ('a, 'b) prod = [%import: ('a, 'b) Datatypes.prod]
+  [@@deriving sexp]
+  type 'a list = [%import: 'a Datatypes.list]
   [@@deriving sexp]
 end
 module Integers = struct
@@ -34,7 +23,35 @@ module MyInts = struct
   [@@deriving sexp]
 end
 
-include SmtExpr
-type coq_SmtBoolExpr = [%import: SmtExpr.coq_SmtBoolExpr]
-and coq_SmtArithExpr = [%import: SmtExpr.coq_SmtArithExpr]
+module CrIdentifiers = struct
+  include CrIdentifiers
+  type coq_Header = [%import: CrIdentifiers.coq_Header]
+  [@@deriving sexp]
+  type coq_State = [%import: CrIdentifiers.coq_State]
+  [@@deriving sexp]
+  type coq_Ctrl = [%import: CrIdentifiers.coq_Ctrl]
+  [@@deriving sexp]
+end
+module CrTransformer = struct
+  include CrTransformer
+  type coq_FunctionArgument = [%import: CrTransformer.coq_FunctionArgument]
+  [@@deriving sexp]
+  type coq_BinaryOp = [%import: CrTransformer.coq_BinaryOp]
+  [@@deriving sexp]
+  type coq_HdrOp = [%import: CrTransformer.coq_HdrOp]
+  [@@deriving sexp]
+  type coq_MatchPattern = [%import: CrTransformer.coq_MatchPattern]
+  [@@deriving sexp]
+  type coq_SeqRule = [%import: CrTransformer.coq_SeqRule]
+  [@@deriving sexp]
+  type coq_ParRule = [%import: CrTransformer.coq_ParRule]
+  [@@deriving sexp]
+  type coq_MatchActionRule = [%import: CrTransformer.coq_MatchActionRule]
+  [@@deriving sexp]
+  type coq_Transformer = [%import: CrTransformer.coq_Transformer]
+  [@@deriving sexp]
+end
+
+include CrDsl
+type coq_CaracaraProgram = [%import: CrDsl.coq_CaracaraProgram]
 [@@deriving sexp]
--- extracted_code/Z3Solver.ml
+++ extracted_code/Z3Solver.ml
@@ -2,34 +2,34 @@ open Z3
 
 (* Recursively convert a coq_SmtBoolExpr to a Z3 expression *)
 (* TODO: This function is trusted, so needs to be checked via other means like fuzzing *)
-let rec z3_expr_from_coq_smt_bool_expr (expr : Interface.coq_SmtBoolExpr) (ctx : Z3.context)
+let rec z3_expr_from_coq_smt_bool_expr (expr : SmtExpr.coq_SmtBoolExpr) (ctx : Z3.context)
   : Z3.Expr.expr =
   match expr with
-  | Interface.SmtTrue -> Z3.Boolean.mk_true ctx
-  | Interface.SmtFalse -> Z3.Boolean.mk_false ctx
-  | Interface.SmtBoolAnd (e1, e2) -> Z3.Boolean.mk_and ctx [z3_expr_from_coq_smt_bool_expr e1 ctx; z3_expr_from_coq_smt_bool_expr e2 ctx]
-  | Interface.SmtBoolOr (e1, e2) -> Z3.Boolean.mk_or ctx [z3_expr_from_coq_smt_bool_expr e1 ctx; z3_expr_from_coq_smt_bool_expr e2 ctx]
-  | Interface.SmtBoolNot e -> Z3.Boolean.mk_not ctx (z3_expr_from_coq_smt_bool_expr e ctx)
-  | Interface.SmtBoolEq (a1, a2) -> Z3.Boolean.mk_eq ctx (z3_expr_from_coq_smt_arith_expr a1 ctx) (z3_expr_from_coq_smt_arith_expr a2 ctx)
-and z3_expr_from_coq_smt_arith_expr (expr : Interface.coq_SmtArithExpr) (ctx : Z3.context)
+  | SmtExpr.SmtTrue -> Z3.Boolean.mk_true ctx
+  | SmtExpr.SmtFalse -> Z3.Boolean.mk_false ctx
+  | SmtExpr.SmtBoolAnd (e1, e2) -> Z3.Boolean.mk_and ctx [z3_expr_from_coq_smt_bool_expr e1 ctx; z3_expr_from_coq_smt_bool_expr e2 ctx]
+  | SmtExpr.SmtBoolOr (e1, e2) -> Z3.Boolean.mk_or ctx [z3_expr_from_coq_smt_bool_expr e1 ctx; z3_expr_from_coq_smt_bool_expr e2 ctx]
+  | SmtExpr.SmtBoolNot e -> Z3.Boolean.mk_not ctx (z3_expr_from_coq_smt_bool_expr e ctx)
+  | SmtExpr.SmtBoolEq (a1, a2) -> Z3.Boolean.mk_eq ctx (z3_expr_from_coq_smt_arith_expr a1 ctx) (z3_expr_from_coq_smt_arith_expr a2 ctx)
+and z3_expr_from_coq_smt_arith_expr (expr : SmtExpr.coq_SmtArithExpr) (ctx : Z3.context)
   : Z3.Expr.expr =
   match expr with
-  | Interface.SmtConst n -> Z3.Arithmetic.Integer.mk_numeral_i ctx (Obj.magic n : int) (* TODO: Obj.magic is not type safe. *)
-  | Interface.SmtArithVar name -> Z3.Arithmetic.Integer.mk_const ctx (Z3.Symbol.mk_string ctx (Obj.magic name : string))
-  | Interface.SmtConditional (cond, e1, e2) ->
+  | SmtExpr.SmtConst n -> Z3.Arithmetic.Integer.mk_numeral_i ctx (Obj.magic n : int) (* TODO: Obj.magic is not type safe. *)
+  | SmtExpr.SmtArithVar name -> Z3.Arithmetic.Integer.mk_const ctx (Z3.Symbol.mk_string ctx (Obj.magic name : string))
+  | SmtExpr.SmtConditional (cond, e1, e2) ->
       Z3.Boolean.mk_ite ctx (z3_expr_from_coq_smt_bool_expr cond ctx) (z3_expr_from_coq_smt_arith_expr e1 ctx) (z3_expr_from_coq_smt_arith_expr e2 ctx)
-  | Interface.SmtBitAdd (e1, e2) -> Z3.Arithmetic.mk_add ctx [z3_expr_from_coq_smt_arith_expr e1 ctx; z3_expr_from_coq_smt_arith_expr e2 ctx]
-  | Interface.SmtBitSub (e1, e2) -> Z3.Arithmetic.mk_sub ctx [z3_expr_from_coq_smt_arith_expr e1 ctx; z3_expr_from_coq_smt_arith_expr e2 ctx]
-  | Interface.SmtBitAnd (e1, e2) -> Z3.BitVector.mk_and ctx (z3_expr_from_coq_smt_arith_expr e1 ctx) (z3_expr_from_coq_smt_arith_expr e2 ctx)
-  | Interface.SmtBitOr (e1, e2)  -> Z3.BitVector.mk_or ctx (z3_expr_from_coq_smt_arith_expr e1 ctx) (z3_expr_from_coq_smt_arith_expr e2 ctx)
-  | Interface.SmtBitXor (e1, e2) -> Z3.BitVector.mk_xor ctx (z3_expr_from_coq_smt_arith_expr e1 ctx) (z3_expr_from_coq_smt_arith_expr e2 ctx)
-  | Interface.SmtBitNot e        -> Z3.BitVector.mk_not ctx (z3_expr_from_coq_smt_arith_expr e ctx)
-  | Interface.SmtBitMul (e1, e2) -> Z3.Arithmetic.mk_mul ctx [z3_expr_from_coq_smt_arith_expr e1 ctx; z3_expr_from_coq_smt_arith_expr e2 ctx]
-  | Interface.SmtBitDiv (e1, e2) -> Z3.Arithmetic.mk_div ctx (z3_expr_from_coq_smt_arith_expr e1 ctx) (z3_expr_from_coq_smt_arith_expr e2 ctx)
-  | Interface.SmtBitMod (e1, e2) -> Z3.Arithmetic.Integer.mk_mod ctx (z3_expr_from_coq_smt_arith_expr e1 ctx) (z3_expr_from_coq_smt_arith_expr e2 ctx)
+  | SmtExpr.SmtBitAdd (e1, e2) -> Z3.Arithmetic.mk_add ctx [z3_expr_from_coq_smt_arith_expr e1 ctx; z3_expr_from_coq_smt_arith_expr e2 ctx]
+  | SmtExpr.SmtBitSub (e1, e2) -> Z3.Arithmetic.mk_sub ctx [z3_expr_from_coq_smt_arith_expr e1 ctx; z3_expr_from_coq_smt_arith_expr e2 ctx]
+  | SmtExpr.SmtBitAnd (e1, e2) -> Z3.BitVector.mk_and ctx (z3_expr_from_coq_smt_arith_expr e1 ctx) (z3_expr_from_coq_smt_arith_expr e2 ctx)
+  | SmtExpr.SmtBitOr (e1, e2)  -> Z3.BitVector.mk_or ctx (z3_expr_from_coq_smt_arith_expr e1 ctx) (z3_expr_from_coq_smt_arith_expr e2 ctx)
+  | SmtExpr.SmtBitXor (e1, e2) -> Z3.BitVector.mk_xor ctx (z3_expr_from_coq_smt_arith_expr e1 ctx) (z3_expr_from_coq_smt_arith_expr e2 ctx)
+  | SmtExpr.SmtBitNot e        -> Z3.BitVector.mk_not ctx (z3_expr_from_coq_smt_arith_expr e ctx)
+  | SmtExpr.SmtBitMul (e1, e2) -> Z3.Arithmetic.mk_mul ctx [z3_expr_from_coq_smt_arith_expr e1 ctx; z3_expr_from_coq_smt_arith_expr e2 ctx]
+  | SmtExpr.SmtBitDiv (e1, e2) -> Z3.Arithmetic.mk_div ctx (z3_expr_from_coq_smt_arith_expr e1 ctx) (z3_expr_from_coq_smt_arith_expr e2 ctx)
+  | SmtExpr.SmtBitMod (e1, e2) -> Z3.Arithmetic.Integer.mk_mod ctx (z3_expr_from_coq_smt_arith_expr e1 ctx) (z3_expr_from_coq_smt_arith_expr e2 ctx)
 
 (* Process an SmtBoolExpr from SmtExpr to generate a query for Z3 using the OCaml Z3 API *)
-let solve (expr : Interface.coq_SmtBoolExpr) =
+let solve (expr : SmtExpr.coq_SmtBoolExpr) =
   let ctx = mk_context [] in
   let solver = Solver.mk_solver ctx None in 
   Solver.add solver [z3_expr_from_coq_smt_bool_expr expr ctx];
@@ -41,34 +41,14 @@ let solve (expr : Interface.coq_SmtBoolExpr) =
   | Z3.Solver.UNKNOWN -> SmtTypes.SmtUnknown
 
 let ecz () =
-  let simplest_state : SmtExpr.coq_SmtArithExpr CrProgramState.coq_ProgramState = {
-    ctrl_map = Coq_pair (SmtExpr.SmtConst (Obj.magic 0 : MyInts.uint8), Maps.PTree.empty);
-    header_map = Coq_pair (SmtExpr.SmtConst (Obj.magic 0 : MyInts.uint8), Maps.PTree.empty);
-    state_map = Coq_pair (SmtExpr.SmtConst (Obj.magic 0 : MyInts.uint8), Maps.PTree.empty)
-  } in
-  match simplest_state with
-  | { ctrl_map = Coq_pair (SmtExpr.SmtConst n, _); _ } ->
-      Printf.printf "ctrl_map constant: %d\n" (Obj.magic n : int)
-  | _ -> print_endline "No match"
+  let h_b : BinNums.positive = Coq_xO Coq_xH in
+  match (h_b, 1) with
+  | _ -> print_endline "incomplete"
 
 (* Main function to call the function solve above *)
 let () =
-  let expr = Interface.SmtTrue in
+  let expr = SmtExpr.SmtTrue in
   match solve expr with
   | SmtTypes.SmtUnsat -> print_endline "UNSATISFIABLE"
   | SmtTypes.SmtSat _ -> print_endline "SATISFIABLE"
   | SmtTypes.SmtUnknown -> print_endline "UNKNOWN"
-
-(* 
-Definition headers_to_check : list Header := [first_generated.header_H; second_generated.header_H].
-Definition state_vars_to_check : list StateVar := [].
-Definition my_equivalence_check : SmtResult :=
-  equivalence_checker
-      simplest_state       (* Starting symbolic state *)
-      first_generated.the_table_0_seq_rule          (* First rule *)
-      second_generated.the_table_0_seq_rule          (* Second rule (same) *)
-      headers_to_check             
-      state_vars_to_check.   
-
-Eval compute in my_equivalence_check.
-*)
