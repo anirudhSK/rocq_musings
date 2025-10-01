663c58f2f06c573d9a7678b10d564a49b2dbe818
seenry <s.rhee@nyu.edu>
2025-09-30 20:08:21 -0400

    Added type interface for sexplib
    Co-Authored-By: Anirudh Sivaraman <sk.anirudh@gmail.com>
    Co-Authored-By: Joseph Tassarotti <jt4767@nyu.edu>

--- Extraction.v
+++ Extraction.v
@@ -2,6 +2,7 @@ From Coq Require Extraction.
 Extraction Language OCaml.
 
 From MyProject Require Import SmtQuery.
+From MyProject Require Import Integers.
 
 (* Tell extraction to use your external OCaml implementation *)
 Extract Constant smt_query => "Z3Solver.solve".
@@ -9,4 +10,4 @@ Extract Constant smt_query => "Z3Solver.solve".
 Set Extraction Output Directory "extracted_code".
 
 (* Extract everything else normally *)
-Separate Extraction SmtQuery.equivalence_checker_cr_dsl.
+Separate Extraction CrDsl.CaracaraProgram Integers.repr SmtQuery.equivalence_checker_cr_dsl SmtTypes.SmtResult.
