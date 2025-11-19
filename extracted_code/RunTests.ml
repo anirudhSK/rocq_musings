443ba553368eb3a37db70114d1aa81be8403b3ad
seenry <s.rhee@nyu.edu>
2025-11-19 18:59:56 -0500

    slightly easier test registration

--- extracted_code/RunTests.ml
+++ extracted_code/RunTests.ml
@@ -14,55 +14,53 @@ let programs = [|
   get_program "./test/subtract2.out"
 |]
 
-let test_1 () =
-  (* reflexive comparison of extremely basic program *)
+let tests = ref []
+let register test_label test_fn =
+  tests := (test_label, test_fn) :: !tests
+
+let () = register "refl_0" (fun () ->
   let p = programs.(0) in
 
   let res = SmtQuery.equivalence_checker_cr_dsl p p in
   match res with
   | Equivalent -> 1
-  | _ -> 0
+  | _ -> 0)
 
-let test_2 () =
-  (* compares two basic programs with different header values *)
+let () = register "hdr_diff" (fun () ->
   let p1 = programs.(0) in
   let p2 = programs.(1) in
 
   let res = SmtQuery.equivalence_checker_cr_dsl p1 p2 in
   match res with
   | NotEquivalent _ -> 1
-  | _ -> 0
+  | _ -> 0)
 
-let test_3 () =
-  (* reflexive comparison of subtraction *)
+let () = register "refl_1" (fun () ->
   let p = programs.(2) in
 
   let res = SmtQuery.equivalence_checker_cr_dsl p p in
   match res with
   | Equivalent -> 1
-  | _ -> 0
+  | _ -> 0)
 
-let test_4 () =
-  (* compares subtraction and 1s complement *)
+let () = register "sub_1comp" (fun () ->
   let p1 = programs.(2) in
   let p2 = programs.(3) in
 
   let res = SmtQuery.equivalence_checker_cr_dsl p1 p2 in
   match res with
   | Equivalent -> 1
-  | _ -> 0
+  | _ -> 0)
 
 let () =
-  let tests = [
-    test_1; test_2; test_3; test_4
-  ] in
+  let test_stats = Stdlib.List.fold_left
+    (fun (acc : int * int) (name, test) -> (
+      let p, t = acc in
+      Printf.printf "Running Test (%s)\n" name;
+      let passed = test () in (
+      Printf.printf "\027[1m%s\027[0m\n\n" (if passed = 1 then "\027[32mPASSED" else "\027[31mFAILED");
+      (p + passed, t + 1)))) (0, 0) (Stdlib.List.rev !tests) in
   
-  let n_passed = Stdlib.List.fold_left
-    (fun (acc : int) test ->
-      let passed = test () in
-      acc + passed) 0 tests in
-  
-  let n_tests = Stdlib.List.length tests in
-  Printf.printf "Passed %d/%d tests\n" n_passed n_tests;
+  (Printf.printf "Passed %d/%d tests\n" (fst test_stats) (snd test_stats);
 
-  if n_passed <> n_tests then exit 1
+  if ((fst test_stats) <> (snd test_stats)) then exit 1)
