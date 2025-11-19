cf3f4107c0c8a09a49307ba9809f131c00b4faf6
LanderDuncan <134824437+LanderDuncan@users.noreply.github.com>
2025-11-19 13:47:56 -0500

    test adding subtraction tests

--- extracted_code/RunTests.ml
+++ extracted_code/RunTests.ml
@@ -9,7 +9,9 @@ let get_program f =
 
 let programs = [|
   get_program "./test/prog1.out";
-  get_program "./test/prog2.out"
+  get_program "./test/prog2.out";
+  get_program "./test/subtract1.out";
+  get_program "./test/subtract2.out"
 |]
 
 let test_1 () =
@@ -31,9 +33,28 @@ let test_2 () =
   | NotEquivalent _ -> 1
   | _ -> 0
 
+let test_3 () =
+  (* reflexive comparison of extremely basic program *)
+  let p = programs.(2) in
+
+  let res = SmtQuery.equivalence_checker_cr_dsl p p in
+  match res with
+  | Equivalent -> 1
+  | _ -> 0
+
+let test_4 () =
+  (* compares two basic programs with different header values *)
+  let p1 = programs.(2) in
+  let p2 = programs.(3) in
+
+  let res = SmtQuery.equivalence_checker_cr_dsl p1 p2 in
+  match res with
+  | NotEquivalent _ -> 1
+  | _ -> 0
+
 let () =
   let tests = [
-    test_1; test_2
+    test_1; test_2; test_3; test_4
   ] in
   
   let n_passed = Stdlib.List.fold_left
