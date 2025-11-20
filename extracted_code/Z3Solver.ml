59192d86a5337c7ed01556462ec862192967cc24
seenry <s.rhee@nyu.edu>
2025-11-20 10:02:07 -0500

    rocq ints are z3 bitvecs

--- extracted_code/Z3Solver.ml
+++ extracted_code/Z3Solver.ml
@@ -3,7 +3,7 @@ open Z3
 
 (* LLM's recommended type format for uniquely tracking variables *)
 module CoqStringOrd = struct
-  type t = string
+  type t = Stdlib.String.t
   let compare = Stdlib.String.compare
 end
 module StringMap = Stdlib.Map.Make(CoqStringOrd)
@@ -21,44 +21,39 @@ let rec z3_expr_from_coq_smt_bool_expr (expr : SmtExpr.coq_SmtBoolExpr) (ctx : Z
 and z3_expr_from_coq_smt_arith_expr (expr : SmtExpr.coq_SmtArithExpr) (ctx : Z3.context) (vars : var_tracker)
   : Z3.Expr.expr =
   match expr with
-  | SmtExpr.SmtConst n -> Z3.Arithmetic.Integer.mk_numeral_i ctx (Interface.coq_Z_to_int n)
+  | SmtExpr.SmtConst n -> Z3.BitVector.mk_numeral ctx (string_of_int (Interface.coq_Z_to_int n)) 8
   | SmtExpr.SmtArithVar name -> (
     let name_str = Interface.coq_str_to_str name in
     match StringMap.find_opt name_str !vars with
     | Some z3_var -> z3_var
     | None ->
-        let z3_var = Z3.Arithmetic.Integer.mk_const ctx (Z3.Symbol.mk_string ctx name_str) in
+        let z3_var = Z3.BitVector.mk_const ctx (Z3.Symbol.mk_string ctx name_str) 8 in
         vars := StringMap.add name_str z3_var !vars;
         z3_var)
   | SmtExpr.SmtConditional (cond, e1, e2) ->
       Z3.Boolean.mk_ite ctx (z3_expr_from_coq_smt_bool_expr cond ctx vars) (z3_expr_from_coq_smt_arith_expr e1 ctx vars) (z3_expr_from_coq_smt_arith_expr e2 ctx vars)
-  | SmtExpr.SmtBitAdd (e1, e2) -> Z3.Arithmetic.mk_add ctx [z3_expr_from_coq_smt_arith_expr e1 ctx vars; z3_expr_from_coq_smt_arith_expr e2 ctx vars]
-  | SmtExpr.SmtBitSub (e1, e2) -> Z3.Arithmetic.mk_sub ctx [z3_expr_from_coq_smt_arith_expr e1 ctx vars; z3_expr_from_coq_smt_arith_expr e2 ctx vars]
+  | SmtExpr.SmtBitAdd (e1, e2) -> Z3.BitVector.mk_add ctx (z3_expr_from_coq_smt_arith_expr e1 ctx vars) (z3_expr_from_coq_smt_arith_expr e2 ctx vars)
+  | SmtExpr.SmtBitSub (e1, e2) -> Z3.BitVector.mk_sub ctx (z3_expr_from_coq_smt_arith_expr e1 ctx vars) (z3_expr_from_coq_smt_arith_expr e2 ctx vars)
   | SmtExpr.SmtBitAnd (e1, e2) -> Z3.BitVector.mk_and ctx (z3_expr_from_coq_smt_arith_expr e1 ctx vars) (z3_expr_from_coq_smt_arith_expr e2 ctx vars)
   | SmtExpr.SmtBitOr (e1, e2)  -> Z3.BitVector.mk_or ctx (z3_expr_from_coq_smt_arith_expr e1 ctx vars) (z3_expr_from_coq_smt_arith_expr e2 ctx vars)
   | SmtExpr.SmtBitXor (e1, e2) -> Z3.BitVector.mk_xor ctx (z3_expr_from_coq_smt_arith_expr e1 ctx vars) (z3_expr_from_coq_smt_arith_expr e2 ctx vars)
   | SmtExpr.SmtBitNot e        -> Z3.BitVector.mk_not ctx (z3_expr_from_coq_smt_arith_expr e ctx vars)
-  | SmtExpr.SmtBitMul (e1, e2) -> Z3.Arithmetic.mk_mul ctx [z3_expr_from_coq_smt_arith_expr e1 ctx vars; z3_expr_from_coq_smt_arith_expr e2 ctx vars]
-  | SmtExpr.SmtBitDiv (e1, e2) -> Z3.Arithmetic.mk_div ctx (z3_expr_from_coq_smt_arith_expr e1 ctx vars) (z3_expr_from_coq_smt_arith_expr e2 ctx vars)
-  | SmtExpr.SmtBitMod (e1, e2) -> Z3.Arithmetic.Integer.mk_mod ctx (z3_expr_from_coq_smt_arith_expr e1 ctx vars) (z3_expr_from_coq_smt_arith_expr e2 ctx vars)
+  | SmtExpr.SmtBitMul (e1, e2) -> Z3.BitVector.mk_mul ctx (z3_expr_from_coq_smt_arith_expr e1 ctx vars) (z3_expr_from_coq_smt_arith_expr e2 ctx vars)
+  | SmtExpr.SmtBitDiv (e1, e2) -> Z3.BitVector.mk_udiv ctx (z3_expr_from_coq_smt_arith_expr e1 ctx vars) (z3_expr_from_coq_smt_arith_expr e2 ctx vars)
+  | SmtExpr.SmtBitMod (e1, e2) -> Z3.BitVector.mk_smod ctx (z3_expr_from_coq_smt_arith_expr e1 ctx vars) (z3_expr_from_coq_smt_arith_expr e2 ctx vars)
 
-(* Gets all variable assignments and folds into a valuation (linked list) *)
+(* Gets all variable assignments and folds them into a valuation (linked list) *)
 let to_vmap (m : Z3.Model.model) (acc : Interface.coq_ValueMap) (name : string) (z3_var : Z3.Expr.expr) : Interface.coq_ValueMap =
   match Z3.Model.eval m z3_var true with
   | Some v ->
     if Z3.Expr.is_numeral v then
-      let x = Z3.Arithmetic.Integer.get_big_int v in
-      if (not ((Z.gt x (Z.of_int 255))
-            || (Z.lt x Z.zero))) then
-        let value = Z.to_int x in (
-        Printf.printf "| \027[1m%s\027[0m = %d\n" name value;
-        Interface.VMap (
-          Interface.str_to_coq_str name,
-          Interface.int_to_coq_uint8 value,
-          acc
-        ))
-      else
-        raise (Failure ("Expects uint8 but got OoB value for " ^ name))
+      let var_str = Z3.BitVector.numeral_to_string v in
+      let var_val = int_of_string var_str in
+      Printf.printf "| \027[1m%s\027[0m = %d\n" name var_val;
+      Interface.VMap (
+        Interface.str_to_coq_str name,
+        Interface.int_to_coq_uint8 var_val,
+        acc)
     else
       raise (Failure ("Expects uint8 but got non-numeral value for " ^ name))
   | None -> raise (Failure ("Z3 failed to return valuation for " ^ name))
