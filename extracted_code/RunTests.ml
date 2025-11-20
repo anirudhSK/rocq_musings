fc2e6cf71fe106a0a1ad97013004307602d7136a
Lander Duncan <134824437+LanderDuncan@users.noreply.github.com>
2025-11-20 17:00:54 -0500

    Update p4c compiler with support for a few more P4 constructs and add readme submodule fix (#29)
    * Update compiler and add readme submodule fix
    
    * test adding subtraction tests
    
    * source p4 files
    
    * fixed test case issue
    
    * fixed byte ordering in type converter
    
    * slightly easier test registration
    
    * fix incorrect SMT variable tracking
    
    * rocq ints are z3 bitvecs
    
    * made valuation print out more legible
    
    ---------
    
    Co-authored-by: seenry <s.rhee@nyu.edu>

--- extracted_code/RunTests.ml
+++ extracted_code/RunTests.ml
@@ -9,39 +9,58 @@ let get_program f =
 
 let programs = [|
   get_program "./test/prog1.out";
-  get_program "./test/prog2.out"
+  get_program "./test/prog2.out";
+  get_program "./test/subtract1.out";
+  get_program "./test/subtract2.out"
 |]
 
-let test_1 () =
-  (* reflexive comparison of extremely basic program *)
+let tests = ref []
+let register test_label test_fn =
+  tests := (test_label, test_fn) :: !tests
+
+let () = register "refl_0" (fun () ->
   let p = programs.(0) in
 
   let res = SmtQuery.equivalence_checker_cr_dsl p p in
   match res with
   | Equivalent -> 1
-  | _ -> 0
+  | _ -> 0)
 
-let test_2 () =
-  (* compares two basic programs with different header values *)
+let () = register "hdr_diff" (fun () ->
   let p1 = programs.(0) in
   let p2 = programs.(1) in
 
   let res = SmtQuery.equivalence_checker_cr_dsl p1 p2 in
   match res with
   | NotEquivalent _ -> 1
-  | _ -> 0
+  | _ -> 0)
+
+let () = register "refl_1" (fun () ->
+  let p = programs.(2) in
+
+  let res = SmtQuery.equivalence_checker_cr_dsl p p in
+  match res with
+  | Equivalent -> 1
+  | _ -> 0)
+
+let () = register "sub_1comp" (fun () ->
+  let p1 = programs.(2) in
+  let p2 = programs.(3) in
+
+  let res = SmtQuery.equivalence_checker_cr_dsl p1 p2 in
+  match res with
+  | Equivalent -> 1
+  | _ -> 0)
 
 let () =
-  let tests = [
-    test_1; test_2
-  ] in
+  let test_stats = Stdlib.List.fold_left
+    (fun (acc : int * int) (name, test) -> (
+      let p, t = acc in
+      Printf.printf "Running Test (%s)\n" name;
+      let passed = test () in (
+      Printf.printf "\027[1m%s\027[0m\n\n" (if passed = 1 then "\027[32mPASSED" else "\027[31mFAILED");
+      (p + passed, t + 1)))) (0, 0) (Stdlib.List.rev !tests) in
   
-  let n_passed = Stdlib.List.fold_left
-    (fun (acc : int) test ->
-      let passed = test () in
-      acc + passed) 0 tests in
-  
-  let n_tests = Stdlib.List.length tests in
-  Printf.printf "Passed %d/%d tests\n" n_passed n_tests;
+  (Printf.printf "Passed %d/%d tests\n" (fst test_stats) (snd test_stats);
 
-  if n_passed <> n_tests then exit 1
+  if ((fst test_stats) <> (snd test_stats)) then exit 1)
--- extracted_code/Z3Solver.ml
+++ extracted_code/Z3Solver.ml
@@ -1,78 +1,86 @@
 open Char
 open Z3
 
-(* Recursively convert a coq_SmtBoolExpr to a Z3 expression *)
-(* TODO: This function is trusted, so needs to be checked via other means like fuzzing *)
-let rec z3_expr_from_coq_smt_bool_expr (expr : SmtExpr.coq_SmtBoolExpr) (ctx : Z3.context)
+(* LLM's recommended type format for uniquely tracking variables *)
+module CoqStringOrd = struct
+  type t = Stdlib.String.t
+  let compare = Stdlib.String.compare
+end
+module StringMap = Stdlib.Map.Make(CoqStringOrd)
+type var_tracker = Z3.Expr.expr StringMap.t ref
+
+let rec z3_expr_from_coq_smt_bool_expr (expr : SmtExpr.coq_SmtBoolExpr) (ctx : Z3.context) (vars : var_tracker)
   : Z3.Expr.expr =
   match expr with
   | SmtExpr.SmtTrue -> Z3.Boolean.mk_true ctx
   | SmtExpr.SmtFalse -> Z3.Boolean.mk_false ctx
-  | SmtExpr.SmtBoolAnd (e1, e2) -> Z3.Boolean.mk_and ctx [z3_expr_from_coq_smt_bool_expr e1 ctx; z3_expr_from_coq_smt_bool_expr e2 ctx]
-  | SmtExpr.SmtBoolOr (e1, e2) -> Z3.Boolean.mk_or ctx [z3_expr_from_coq_smt_bool_expr e1 ctx; z3_expr_from_coq_smt_bool_expr e2 ctx]
-  | SmtExpr.SmtBoolNot e -> Z3.Boolean.mk_not ctx (z3_expr_from_coq_smt_bool_expr e ctx)
-  | SmtExpr.SmtBoolEq (a1, a2) -> Z3.Boolean.mk_eq ctx (z3_expr_from_coq_smt_arith_expr a1 ctx) (z3_expr_from_coq_smt_arith_expr a2 ctx)
-and z3_expr_from_coq_smt_arith_expr (expr : SmtExpr.coq_SmtArithExpr) (ctx : Z3.context)
+  | SmtExpr.SmtBoolAnd (e1, e2) -> Z3.Boolean.mk_and ctx [z3_expr_from_coq_smt_bool_expr e1 ctx vars; z3_expr_from_coq_smt_bool_expr e2 ctx vars]
+  | SmtExpr.SmtBoolOr (e1, e2) -> Z3.Boolean.mk_or ctx [z3_expr_from_coq_smt_bool_expr e1 ctx vars; z3_expr_from_coq_smt_bool_expr e2 ctx vars]
+  | SmtExpr.SmtBoolNot e -> Z3.Boolean.mk_not ctx (z3_expr_from_coq_smt_bool_expr e ctx vars)
+  | SmtExpr.SmtBoolEq (a1, a2) -> Z3.Boolean.mk_eq ctx (z3_expr_from_coq_smt_arith_expr a1 ctx vars) (z3_expr_from_coq_smt_arith_expr a2 ctx vars)
+and z3_expr_from_coq_smt_arith_expr (expr : SmtExpr.coq_SmtArithExpr) (ctx : Z3.context) (vars : var_tracker)
   : Z3.Expr.expr =
   match expr with
-  | SmtExpr.SmtConst n -> Z3.Arithmetic.Integer.mk_numeral_i ctx (Interface.coq_Z_to_int n)
-  | SmtExpr.SmtArithVar name -> Z3.Arithmetic.Integer.mk_const ctx (Z3.Symbol.mk_string ctx (Interface.coq_str_to_str name))
+  | SmtExpr.SmtConst n -> Z3.BitVector.mk_numeral ctx (string_of_int (Interface.coq_Z_to_int n)) 8
+  | SmtExpr.SmtArithVar name -> (
+    let name_str = Interface.coq_str_to_str name in
+    match StringMap.find_opt name_str !vars with
+    | Some z3_var -> z3_var
+    | None ->
+        let z3_var = Z3.BitVector.mk_const ctx (Z3.Symbol.mk_string ctx name_str) 8 in
+        vars := StringMap.add name_str z3_var !vars;
+        z3_var)
   | SmtExpr.SmtConditional (cond, e1, e2) ->
-      Z3.Boolean.mk_ite ctx (z3_expr_from_coq_smt_bool_expr cond ctx) (z3_expr_from_coq_smt_arith_expr e1 ctx) (z3_expr_from_coq_smt_arith_expr e2 ctx)
-  | SmtExpr.SmtBitAdd (e1, e2) -> Z3.Arithmetic.mk_add ctx [z3_expr_from_coq_smt_arith_expr e1 ctx; z3_expr_from_coq_smt_arith_expr e2 ctx]
-  | SmtExpr.SmtBitSub (e1, e2) -> Z3.Arithmetic.mk_sub ctx [z3_expr_from_coq_smt_arith_expr e1 ctx; z3_expr_from_coq_smt_arith_expr e2 ctx]
-  | SmtExpr.SmtBitAnd (e1, e2) -> Z3.BitVector.mk_and ctx (z3_expr_from_coq_smt_arith_expr e1 ctx) (z3_expr_from_coq_smt_arith_expr e2 ctx)
-  | SmtExpr.SmtBitOr (e1, e2)  -> Z3.BitVector.mk_or ctx (z3_expr_from_coq_smt_arith_expr e1 ctx) (z3_expr_from_coq_smt_arith_expr e2 ctx)
-  | SmtExpr.SmtBitXor (e1, e2) -> Z3.BitVector.mk_xor ctx (z3_expr_from_coq_smt_arith_expr e1 ctx) (z3_expr_from_coq_smt_arith_expr e2 ctx)
-  | SmtExpr.SmtBitNot e        -> Z3.BitVector.mk_not ctx (z3_expr_from_coq_smt_arith_expr e ctx)
-  | SmtExpr.SmtBitMul (e1, e2) -> Z3.Arithmetic.mk_mul ctx [z3_expr_from_coq_smt_arith_expr e1 ctx; z3_expr_from_coq_smt_arith_expr e2 ctx]
-  | SmtExpr.SmtBitDiv (e1, e2) -> Z3.Arithmetic.mk_div ctx (z3_expr_from_coq_smt_arith_expr e1 ctx) (z3_expr_from_coq_smt_arith_expr e2 ctx)
-  | SmtExpr.SmtBitMod (e1, e2) -> Z3.Arithmetic.Integer.mk_mod ctx (z3_expr_from_coq_smt_arith_expr e1 ctx) (z3_expr_from_coq_smt_arith_expr e2 ctx)
+      Z3.Boolean.mk_ite ctx (z3_expr_from_coq_smt_bool_expr cond ctx vars) (z3_expr_from_coq_smt_arith_expr e1 ctx vars) (z3_expr_from_coq_smt_arith_expr e2 ctx vars)
+  | SmtExpr.SmtBitAdd (e1, e2) -> Z3.BitVector.mk_add ctx (z3_expr_from_coq_smt_arith_expr e1 ctx vars) (z3_expr_from_coq_smt_arith_expr e2 ctx vars)
+  | SmtExpr.SmtBitSub (e1, e2) -> Z3.BitVector.mk_sub ctx (z3_expr_from_coq_smt_arith_expr e1 ctx vars) (z3_expr_from_coq_smt_arith_expr e2 ctx vars)
+  | SmtExpr.SmtBitAnd (e1, e2) -> Z3.BitVector.mk_and ctx (z3_expr_from_coq_smt_arith_expr e1 ctx vars) (z3_expr_from_coq_smt_arith_expr e2 ctx vars)
+  | SmtExpr.SmtBitOr (e1, e2)  -> Z3.BitVector.mk_or ctx (z3_expr_from_coq_smt_arith_expr e1 ctx vars) (z3_expr_from_coq_smt_arith_expr e2 ctx vars)
+  | SmtExpr.SmtBitXor (e1, e2) -> Z3.BitVector.mk_xor ctx (z3_expr_from_coq_smt_arith_expr e1 ctx vars) (z3_expr_from_coq_smt_arith_expr e2 ctx vars)
+  | SmtExpr.SmtBitNot e        -> Z3.BitVector.mk_not ctx (z3_expr_from_coq_smt_arith_expr e ctx vars)
+  | SmtExpr.SmtBitMul (e1, e2) -> Z3.BitVector.mk_mul ctx (z3_expr_from_coq_smt_arith_expr e1 ctx vars) (z3_expr_from_coq_smt_arith_expr e2 ctx vars)
+  | SmtExpr.SmtBitDiv (e1, e2) -> Z3.BitVector.mk_udiv ctx (z3_expr_from_coq_smt_arith_expr e1 ctx vars) (z3_expr_from_coq_smt_arith_expr e2 ctx vars)
+  | SmtExpr.SmtBitMod (e1, e2) -> Z3.BitVector.mk_smod ctx (z3_expr_from_coq_smt_arith_expr e1 ctx vars) (z3_expr_from_coq_smt_arith_expr e2 ctx vars)
 
-let sat_check solver =
+(* Gets all variable assignments and folds them into a valuation (linked list) *)
+let to_vmap (m : Z3.Model.model) (acc : Interface.coq_ValueMap) (name : string) (z3_var : Z3.Expr.expr) : Interface.coq_ValueMap =
+  match Z3.Model.eval m z3_var true with
+  | Some v ->
+    if Z3.Expr.is_numeral v then
+      let var_str = Z3.BitVector.numeral_to_string v in
+      let var_val = int_of_string var_str in
+      Printf.printf "| var( \027[1m%s\027[0m ) := %d\n" name var_val;
+      Interface.VMap (
+        Interface.str_to_coq_str name,
+        Interface.int_to_coq_uint8 var_val,
+        acc)
+    else
+      raise (Failure ("Expects uint8 but got non-numeral value for " ^ name))
+  | None -> raise (Failure ("Z3 failed to return valuation for " ^ name))
+
+let sat_check solver tracked_vars =
   match Solver.check solver [] with
   | Z3.Solver.UNSATISFIABLE -> SmtTypes.SmtUnsat
-  | Z3.Solver.SATISFIABLE ->
-    (
-      let model = Solver.get_model solver in
-      match model with
-      | Some m ->
-        (
-          let variables = Model.get_const_decls m in
-          let valuations = Stdlib.List.fold_left
-            (fun acc const ->
-              let name = Z3.Symbol.to_string (Z3.FuncDecl.get_name const) in
-              match Z3.Model.get_const_interp m const with
-              | Some v ->
-                if Z3.Expr.is_numeral v then
-                  let x = Z3.Arithmetic.Integer.get_big_int v in
-                  if (not ((Z.gt x (Z.of_int 255))
-                        || (Z.lt x Z.zero))) then
-                    let value = Z.to_int x in (
-                    Printf.printf "%s = %d\n" name value;
-                    Interface.VMap (
-                      Interface.str_to_coq_str name,
-                      Interface.int_to_coq_uint8 value,
-                      acc
-                    ))
-                  else
-                    raise (Failure ("Expects uint8 but got OoB value for " ^ name))
-                else
-                  raise (Failure ("Expects uint8 but got non-numeral value for " ^ name))
-              | None -> raise (Failure ("Z3 failed to return valuation for " ^ name)))
-            VMap_DNE
-            variables in
-          SmtTypes.SmtSat (Interface.coq_TraverseMap valuations)
-        )
-      | None -> raise (Failure "Z3 returned SAT, but no valuation.")
-    )
   | Z3.Solver.UNKNOWN -> SmtTypes.SmtUnknown
+  | Z3.Solver.SATISFIABLE -> (
+    let model = Solver.get_model solver in
+    match model with
+    | Some m -> (
+      Printf.printf "┌ SAT Valuation\n";
+      let var_bindings = StringMap.bindings !tracked_vars in
+      let valuations = Stdlib.List.fold_left
+        (fun acc (name, z3_var) -> to_vmap m acc name z3_var)
+        Interface.VMap_DNE
+        var_bindings in
+      Printf.printf "└\n";
+      SmtTypes.SmtSat (Interface.coq_TraverseMap valuations))
+    | None -> raise (Failure "Z3 returned SAT, but no valuation."))
 
-(* Process an SmtBoolExpr from SmtExpr to generate a query for Z3 using the OCaml Z3 API *)
 let solve (expr : SmtExpr.coq_SmtBoolExpr) =
   let ctx = mk_context [] in
-  let solver = Solver.mk_solver ctx None in 
-  Solver.add solver [z3_expr_from_coq_smt_bool_expr expr ctx];
+  let solver = Solver.mk_solver ctx None in
+  let tracked_vars = ref StringMap.empty in
+  let z3_expr = z3_expr_from_coq_smt_bool_expr expr ctx tracked_vars in
+  Solver.add solver [z3_expr];
 
-  (* Check satisfiability of the constraints added to the solver *)
-  sat_check solver
+  sat_check solver tracked_vars
